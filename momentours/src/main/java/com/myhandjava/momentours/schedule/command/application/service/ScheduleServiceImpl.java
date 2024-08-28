6cd56ef96a7342105d5c28a246d77aed0d5d1770
dongkh9 <dongkh99@gamil.com>
2024-08-28 19:44:09 +0900

    refactor: 일정 조회 쿼리 리팩토링
    데이터베이스 구조 변화에 맞추어 일정 조회 기능 쿼리 리팩토링

--- momentours/src/main/java/com/myhandjava/momentours/schedule/command/application/service/ScheduleServiceImpl.java
+++ momentours/src/main/java/com/myhandjava/momentours/schedule/command/application/service/ScheduleServiceImpl.java
@@ -1,7 +1,19 @@
 package com.myhandjava.momentours.schedule.command.application.service;
 
+import com.myhandjava.momentours.schedule.command.domain.aggregate.Schedule;
+import com.myhandjava.momentours.schedule.query.dto.ScheduleDTO;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Service("scheduleCommandServiceImpl")
 public class ScheduleServiceImpl implements ScheduleService {
+
+    @PostMapping("/calendar/{coupleNo}")
+    public List<ScheduleDTO> registSchedule(@PathVariable int coupleNo, ScheduleDTO scheduleDTO){
+        return null;
+    }
 }
