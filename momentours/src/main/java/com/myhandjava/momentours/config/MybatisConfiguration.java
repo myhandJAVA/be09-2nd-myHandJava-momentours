8b193d6114e295ac0deee3cad75cab1e27ed07b8
Vorschlag-bit <bustout@naver.com>
2024-08-29 01:49:05 +0900

    feat:설정 파일 설정 완료 혜진이걸 그대로 복사했는데도 충돌났지만 다시 복사해서 커밋하므로 오류는 없을 겁니다

--- momentours/src/main/java/com/myhandjava/momentours/config/MybatisConfiguration.java
+++ momentours/src/main/java/com/myhandjava/momentours/config/MybatisConfiguration.java
@@ -8,7 +8,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan(basePackages = "com.myhandjava.momentours", annotationClass = Mapper.class)
 public class MybatisConfiguration {
-
-
-
 }
