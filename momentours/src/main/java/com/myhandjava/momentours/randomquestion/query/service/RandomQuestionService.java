29beae85eb75f80c25dbf9284385f85d9eefea4d
이우진 <147306997+Vorschlag-bit@users.noreply.github.com>
2024-08-29 09:05:07 +0900

    Revert "Current randomQuestion 조회"

--- momentours/src/main/java/com/myhandjava/momentours/randomquestion/query/service/RandomQuestionService.java
+++
@@ -1,8 +0,0 @@
-package com.myhandjava.momentours.randomquestion.query.service;
-
-import com.myhandjava.momentours.randomquestion.query.dto.RandomQuestionDTO;
-
-public interface RandomQuestionService {
-
-    RandomQuestionDTO findCurrentRandomQuestionByMemberNo(int userNo);
-}
