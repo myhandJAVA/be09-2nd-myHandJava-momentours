a752fff0965503ecbf972f46d68f8a7358b89e41
yuhyejin <hj_eu@naver.com>
2024-09-02 17:38:44 +0900

    feat: 일기 댓글 조회 일기 조회 시 댓글 + 파일 + 일기 다 나오도록 개발

--- momentours/src/main/java/com/myhandjava/momentours/diary/query/service/DiaryServiceImpl.java
+++ momentours/src/main/java/com/myhandjava/momentours/diary/query/service/DiaryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.myhandjava.momentours.diary.query.service;
 
+import com.myhandjava.momentours.diary.query.dto.CommentDTO;
 import com.myhandjava.momentours.diary.query.dto.DiaryDTO;
 import com.myhandjava.momentours.diary.query.repository.DiaryMapper;
 import com.myhandjava.momentours.file.query.dto.FileDTO;
@@ -26,10 +27,12 @@ public class DiaryServiceImpl implements DiaryService {
         this.fileMapper = fileMapper;
     }
 
-    // 일기 조회
+    // 일기 + 파일 + 댓글 조회
     @Override
     public List<DiaryDTO> selectDiary(DiaryDTO diaryDTO) {
         List<DiaryDTO> result = diaryMapper.selectDiary(diaryDTO);
+
+        System.out.println("result = " + result);
 
         // 입력 문자열 형식에 맞는 DateTimeFormatter 사용
         DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -42,11 +45,30 @@ public class DiaryServiceImpl implements DiaryService {
                     formattedDiary.setDiaryNo(diary.getDiaryNo());
                     formattedDiary.setDiaryContent(diary.getDiaryContent());
 
-                    // 문자열을 LocalDateTime으로 변환하고 포맷팅
+                    // 일기 날짜 포맷팅
                     formattedDiary.setDiaryCreateDate(diary.getDiaryCreateDate() != null ?
                             LocalDateTime.parse(diary.getDiaryCreateDate(), inputFormatter).format(outputFormatter) : null);
                     formattedDiary.setDiaryUpdateDate(diary.getDiaryUpdateDate() != null ?
                             LocalDateTime.parse(diary.getDiaryUpdateDate(), inputFormatter).format(outputFormatter) : null);
+
+                    // 댓글 날짜 포맷팅
+                    List<CommentDTO> formattedComments = diary.getComments().stream()
+                            .map(commnet -> {
+                                CommentDTO commentDTO = new CommentDTO();
+                                commentDTO.setCommentNo(commnet.getCommentNo());
+                                commentDTO.setCommentContent(commnet.getCommentContent());
+
+                                commentDTO.setCommentCreateDate(commnet.getCommentCreateDate() != null ?
+                                        LocalDateTime.parse(commnet.getCommentCreateDate(), inputFormatter).format(outputFormatter) : null);
+                                commentDTO.setCommentUpdateDate(commnet.getCommentUpdateDate() != null ?
+                                        LocalDateTime.parse(commnet.getCommentUpdateDate(), inputFormatter).format(outputFormatter) : null);
+
+                                return commentDTO;
+
+                            })
+                            .collect(Collectors.toList());
+
+                    formattedDiary.setComments(formattedComments);
 
                     List<FileDTO> files = fileMapper.selectFilesByDiaryNo(diary.getDiaryNo());
                     formattedDiary.setFiles(files);
