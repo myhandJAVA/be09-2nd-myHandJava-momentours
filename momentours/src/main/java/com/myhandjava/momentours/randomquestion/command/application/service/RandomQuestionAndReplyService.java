efa3b6970b7301325e6fc776c9b4274da0447acc
이우진 <147306997+Vorschlag-bit@users.noreply.github.com>
2024-09-03 11:08:40 +0900

    Revert "feat: 커플별 고유 랜덤질문 생성 기능 개발"

--- momentours/src/main/java/com/myhandjava/momentours/randomquestion/command/application/service/RandomQuestionAndReplyService.java
+++ momentours/src/main/java/com/myhandjava/momentours/randomquestion/command/application/service/RandomQuestionAndReplyService.java
@@ -3,6 +3,7 @@ package com.myhandjava.momentours.randomquestion.command.application.service;
 import com.myhandjava.momentours.randomquestion.command.application.dto.RandomReplyDTO;
 
 public interface RandomQuestionAndReplyService {
+    void createRandomQuestion(int userNo);
     void removeRandomReply(int replyNo);
     void updateRandomReply(int replyNo, RandomReplyDTO modifyContent);
     void registRandomReply(RandomReplyDTO randomReplyDTO) throws Exception;
