169c741423f175315c3b729f7d8cf83c95cfca70
Vorschlag-bit <bustout@naver.com>
2024-09-03 10:36:45 +0900

    feat: 커플별 고유 랜덤질문 생성 기능 개발

--- momentours/src/main/java/com/myhandjava/momentours/randomquestion/command/application/service/RandomQuestionAndReplyService.java
+++ momentours/src/main/java/com/myhandjava/momentours/randomquestion/command/application/service/RandomQuestionAndReplyService.java
@@ -3,7 +3,6 @@ package com.myhandjava.momentours.randomquestion.command.application.service;
 import com.myhandjava.momentours.randomquestion.command.application.dto.RandomReplyDTO;
 
 public interface RandomQuestionAndReplyService {
-    void createRandomQuestion(int userNo);
     void removeRandomReply(int replyNo);
     void updateRandomReply(int replyNo, RandomReplyDTO modifyContent);
     void registRandomReply(RandomReplyDTO randomReplyDTO) throws Exception;
