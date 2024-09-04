601e1b4df0bd2a92be8088bdc240172be3e8edab
dongkh9 <dongkh99@gamil.com>
2024-09-04 19:13:09 +0900

    feat: 토큰 인터셉터 추가
    컨트롤러 메소드 호출 시 토큰에 있는 정보를 RequestAttribute에 넘겨주는 인터셉터 추가

---
+++ momentours/src/main/java/com/myhandjava/momentours/config/TokenInterceptor.java
@@ -0,0 +1,39 @@
+package com.myhandjava.momentours.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Configuration
+public class TokenInterceptor implements HandlerInterceptor {
+    private Environment env;
+
+    @Autowired
+    public TokenInterceptor(Environment env){
+        this.env = env;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            String token = authorizationHeader.substring(7);
+
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(env.getProperty("token.secretKey"))
+                    .build()
+                    .parseClaimsJws(token);
+
+            request.setAttribute("claims",claims.getBody());
+        }
+
+        return true;
+    }
+}
