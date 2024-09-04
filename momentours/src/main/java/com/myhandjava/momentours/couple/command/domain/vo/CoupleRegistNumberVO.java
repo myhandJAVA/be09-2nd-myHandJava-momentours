059c922fa09f4584cabe11ceb929791e2b8146c5
Vorschlag-bit <https://github.com/Vorschlag-bit/testR.git>
2024-09-04 09:52:41 +0900

    feat: 회원 번호 2개 받아서 커플 정보 입력하는 창으로 안내 후 정보를 입력하면 회원 번호 자동 set까지 구현

---
+++ momentours/src/main/java/com/myhandjava/momentours/couple/command/domain/vo/CoupleRegistNumberVO.java
@@ -0,0 +1,13 @@
+package com.myhandjava.momentours.couple.command.domain.vo;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CoupleRegistNumberVO {
+    private int coupleUserNo1;
+    private int coupleUserNo2;
+}
