fe070d0a468a5f244e37584261198513a7225b5b
Vorschlag-bit <https://github.com/Vorschlag-bit/testR.git>
2024-09-09 17:44:35 +0900

    fix: 일정 푸시 놓친 점 푸시

--- momentours/src/main/java/com/myhandjava/momentours/schedule/command/application/controller/ScheduleController.java
+++ momentours/src/main/java/com/myhandjava/momentours/schedule/command/application/controller/ScheduleController.java
@@ -2,10 +2,9 @@ package com.myhandjava.momentours.schedule.command.application.controller;
 
 import com.myhandjava.momentours.common.ResponseMessage;
 import com.myhandjava.momentours.schedule.command.application.service.ScheduleService;
-import com.myhandjava.momentours.schedule.command.domain.vo.ModifyScheduleVO;
-import com.myhandjava.momentours.schedule.command.domain.vo.RegistScheduleVO;
 import com.myhandjava.momentours.schedule.query.dto.ScheduleDTO;
 import io.jsonwebtoken.Claims;
+import jakarta.ws.rs.PathParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,24 +14,17 @@ import java.util.Map;
 
 
 @RestController("ScheduleCommandController")
-@RequestMapping("calendar")
 public class ScheduleController {
-    private final ScheduleService scheduleService;
+    private ScheduleService scheduleService;
     private ScheduleController(ScheduleService scheduleService){
         this.scheduleService = scheduleService;
     }
 
-    @PostMapping("")
-    public ResponseEntity<ResponseMessage> registSchedule(@RequestBody RegistScheduleVO registScheduleVO,
+    @PostMapping("/calendar")
+    public ResponseEntity<ResponseMessage> registSchedule(@RequestBody ScheduleDTO scheduleDTO,
                                                           @RequestAttribute("claims") Claims claims){
-
-        ScheduleDTO scheduleDTO = new ScheduleDTO();
-        scheduleDTO.setCoupleNo((Integer)claims.get("coupleNo"));
-        scheduleDTO.setScheduleStartDate(registScheduleVO.getScheduleStartDate());
-        scheduleDTO.setScheduleEndDate(registScheduleVO.getScheduleEndDate());
-        scheduleDTO.setScheduleTitle(registScheduleVO.getScheduleTitle());
-        scheduleDTO.setScheduleMemo(registScheduleVO.getScheduleMemo());
-
+        Integer coupleNo = (Integer)claims.get("coupleNo");
+        scheduleDTO.setCoupleNo(coupleNo);
         scheduleService.registSchedule(scheduleDTO);
 
         Map<String,Object> responseMap = new HashMap<>();
@@ -42,43 +34,33 @@ public class ScheduleController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseMessage);
     }
 
-    @PutMapping("/{scheduleNo}")
-    public ResponseEntity<ResponseMessage> modifySchedule(@RequestBody ModifyScheduleVO modifyScheduleVO,
+    @PutMapping("/calendar/{scheduleNo}")
+    public ResponseEntity<ResponseMessage> updateSchedule(@RequestBody ScheduleDTO scheduleDTO,
                                                           @PathVariable int scheduleNo,
                                                           @RequestAttribute("claims") Claims claims){
-        ScheduleDTO scheduleDTO = new ScheduleDTO();
-        scheduleDTO.setCoupleNo((Integer)claims.get("coupleNo"));
-        scheduleDTO.setScheduleStartDate(modifyScheduleVO.getScheduleStartDate());
-        scheduleDTO.setScheduleEndDate(modifyScheduleVO.getScheduleEndDate());
-        scheduleDTO.setScheduleTitle(modifyScheduleVO.getScheduleTitle());
-        scheduleDTO.setScheduleMemo(modifyScheduleVO.getScheduleMemo());
+        Integer coupleNo = (Integer)claims.get("coupleNo");
+        scheduleDTO.setCoupleNo(coupleNo);
         scheduleDTO.setScheduleNo(scheduleNo);
-        scheduleService.modifySchedule(scheduleDTO);
+        scheduleService.updateSchedule(scheduleDTO);
 
         Map<String,Object> responseMap = new HashMap<>();
-        responseMap.put("newSchedule","localhost:8000/calendar");
+        responseMap.put("newSchedule","/calendar/");
 
         ResponseMessage responseMessage = new ResponseMessage(200,"일정 수정 성공", responseMap );
         return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
     }
 
-    @DeleteMapping("/{scheduleNo}")
-    public ResponseEntity<ResponseMessage> removeSchedule(@PathVariable int scheduleNo,
+    @DeleteMapping("/calendar/{scheduleNo}")
+    public ResponseEntity<ResponseMessage> deleteSchedule(@PathVariable int scheduleNo,
                                                           @RequestAttribute("claims") Claims claims){
         ScheduleDTO scheduleDTO = new ScheduleDTO();
         Integer coupleNo = (Integer)claims.get("coupleNo");
-
-        if (coupleNo == null) {
-            ResponseMessage responseMessage = new ResponseMessage(500,"커플번호가 존재안해요",null);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseMessage);
-        }
-
         scheduleDTO.setScheduleNo(scheduleNo);
         scheduleDTO.setCoupleNo(coupleNo);
-        scheduleService.removeSchedule(scheduleDTO);
+        scheduleService.deleteSchedule(scheduleDTO);
 
         Map<String,Object> responseMap = new HashMap<>();
-        responseMap.put("removedSchedule","/calendar/");
+        responseMap.put("deletedSchedule","/calendar/");
 
         ResponseMessage responseMessage = new ResponseMessage(200,"일정 삭제 성공", responseMap );
         return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
